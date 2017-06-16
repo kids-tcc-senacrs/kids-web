a63f5d785e2c5c33538d2d4ce3607b8ea8ab6525
“Luciano <lucianoortizsilva@gmail.com>
2017-06-16 16:19:17 -0300

    #5 configuracao de variavel de ambiente para chamasr API Rest

--- src/app/rest-usuario.service.ts
+++ src/app/rest-usuario.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions} from '@angular/http';
 import {Observable} from 'rxjs';
+import {environment} from '../environments/environment';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/observable/throw';
@@ -9,7 +10,8 @@ import 'rxjs/add/observable/throw';
 @Injectable()
 export class RestUsuarioService {
 
-	private URL_REST_USUARIO:string = 'http://localhost:8080/usuario/';
+	private API : string = 'usuario/'
+	private URL_REST_USUARIO:string =  environment.HOST_KIDS_CORE + this.API;
 
 	constructor(private http:Http) {}
 

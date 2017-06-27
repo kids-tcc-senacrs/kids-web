08a05d8ce5002b174f63d2ef4690d71dd064373b
“Luciano <lucianoortizsilva@gmail.com>
2017-06-26 22:05:39 -0300

    #7 alteraçao nas variaveis de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  HOST_KIDS_CORE : 'http://localhost:8080/',
+  HOST_KIDS_CORE : 'https://kids-core.herokuapp.com/',
   API_GOOGLE_MAP_CEP : 'https://maps.google.com/maps/api/geocode/json?address='
 };
